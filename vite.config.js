cb6217049ec319931eaa2834e524960764273cfb
Marcus-Ayers <marcayers27@gmail.com>
2023-04-21 18:41:25 -0400

    netlify

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { writeFileSync } from "fs";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      output: {
-        // generate a _redirects file in the output directory
-        // with the redirect rule for client-side routing
-        intro: () => {
-          writeFileSync("_redirects", "/*    /index.html   200");
-          return "";
-        },
+      input: {
+        main: "index.html",
       },
     },
+    assetsInlineLimit: 0,
   },
 });
