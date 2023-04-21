d941911623c62e2f72ee0ec9c55d836cf020f424
Marcus-Ayers <marcayers27@gmail.com>
2023-04-21 14:51:00 -0400

    styling for movie page

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -31,30 +31,40 @@ function Movie() {
   }
 
   return (
-    <div className="movie-page">
+    <div>
       <Navbar />
-      <div className="movie-header">
-        <h1>{movieData.title}</h1>
-        <h3>{movieData.release_date}</h3>
-      </div>
-      <div className="movie-details">
-        <div className="movie-poster">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
-            alt={movieData.title}
-          />
+      <div className="movie-page flex flex-col items-center">
+        <div className="movie-header flex flex-col items-center">
+          <h1 className="font-bold">{movieData.title}</h1>
+          <h3 className="font-light text-sm">{movieData.release_date}</h3>
         </div>
-        <div className="movie-info">
-          <h2>Overview</h2>
-          <p>{movieData.overview}</p>
-          <h2>Genres</h2>
-          <ul>
-            {movieData.genres.map((genre) => (
-              <li key={genre.id}>{genre.name}</li>
-            ))}
-          </ul>
-          <h2>Rating</h2>
-          <p>{movieData.vote_average}</p>
+        <div className="movie-details flex flex-col items-center">
+          <div className="movie-poster">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+              alt={movieData.title}
+            />
+          </div>
+          <div className="movie-info flex flex-col items-center">
+            <h2 className="mt-5">Overview</h2>
+            <p className="max-w-md border-b-2 border-slate-800">
+              {movieData.overview}
+            </p>
+            <div className="flex mt-5">
+              <div className="mr-5">
+                <h1 className="font-semibold text-lg">Genres</h1>
+                <ul>
+                  {movieData.genres.map((genre) => (
+                    <li key={genre.id}>{genre.name}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="ml-5">
+                <h2 className="font-semibold text-lg">Rating</h2>
+                <p>{movieData.vote_average}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
